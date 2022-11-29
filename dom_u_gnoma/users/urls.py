b3763bc60d4cd20ac474a0669ec035e03ce9813f
Valeriy Kirichenko <vmkirichenko1991@yandex.ru>
2022-11-29 20:54:18 +0300

    Add password reset and password change, little fixes in templates.

--- dom_u_gnoma/users/urls.py
+++ dom_u_gnoma/users/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from . import views
 
@@ -32,5 +32,40 @@ urlpatterns = [
         views.user_activate,
         name='register_activate'
     ),
-    
+    path(
+        'reset/<uidb64>/<token>/',
+        login_required(auth_views.PasswordResetConfirmView.as_view(
+            success_url=reverse_lazy("login"),
+            template_name="users/password_reset_confirm.html"
+        )),
+        name='password_reset_confirm'
+    ),
+    path(
+        'password_reset/done/',
+        login_required(auth_views.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        )),
+        name='password_reset_done'
+    ),
+    path(
+        'password_reset_form/',
+        login_required(auth_views.PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'
+        )),
+        name='password_reset_form'
+    ),
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'
+        ),
+        name='password_change_form'
+    ),
+    path(
+        'password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'
+        ),
+        name='password_change_done'
+    ),
 ]
